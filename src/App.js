04cf2d23c81f4906732e297108fe0c81c34392d5
“anildalar” <dollorinfotech@gmail.com>
2022-04-18 09:10:47 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  //
+  let state = useSelector((storeObject)=>{
+    return storeObject;
+  });
+
+  let dispatch = useDispatch();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="calculator">
+
+        <input type="text" class="calculator-screen" value={state} disabled />
+
+        <div class="calculator-keys">
+          
+          <button type="button" class="operator" value="+" onClick={()=>{dispatch({type:'ADDITION'})}}>+</button>
+          <button type="button" class="operator" value="-" onClick={()=>{dispatch({type:'SUBTRACTION'})}}>-</button>
+          <button type="button" class="operator" value="*" onClick={()=>{dispatch({type:'MULTIPLICATION'})}}>&times;</button>
+          <button type="button" class="operator" value="/" onClick={()=>{dispatch({type:'DIVISION'})}}>&divide;</button>
+
+          <button type="button" value="7">7</button>
+          <button type="button" value="8">8</button>
+          <button type="button" value="9">9</button>
+
+
+          <button type="button" value="4">4</button>
+          <button type="button" value="5">5</button>
+          <button type="button" value="6">6</button>
+
+
+          <button type="button" value="1">1</button>
+          <button type="button" value="2">2</button>
+          <button type="button" value="3">3</button>
+
+
+          <button type="button" value="0">0</button>
+          <button type="button" class="decimal" value=".">.</button>
+          <button type="button" class="all-clear" value="all-clear" onClick={()=>{dispatch({type:'CLEAR'})}}>AC</button>
+
+          <button type="button" class="equal-sign" value="=" onClick={()=>{dispatch({type:'RESULT'})}}>=</button>
+
+        </div>
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+let rootReducer = (prevState,action)=>{
+  let newState = prevState
+
+  switch(action.type){
+    case 'ADDITION':
+      alert('ADDITION');
+      break;
+    case 'SUBTRACTION':
+      alert('SUBTRACTION');
+      break;
+    case 'MULTIPLICATION':
+      alert('MULTIPLICATION');
+      break;
+    case 'DIVISION':
+      alert('DIVISION');
+      break;
+    case 'RESULT':
+      alert('RESULT');
+      break;
+    case 'CLEAR':
+      alert('CLEAR');
+      break;
+
+    default:
+
+
+  }
+
+  return newState;
+}
+let storeObjecct = createStore(rootReducer,0);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={storeObjecct}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
